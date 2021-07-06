23aaa182729c3989d7a538950073892d3c60adbe
tkgstrator <nasawake.am@gmail.com>
2021-07-07 02:31:11 +0900

    - 記事の追加

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "サーモンラン大全",
+  description:
+    "精霊は謳う。大いなる力、万物の事象を司らん。その生命、その魂、そしてその亡骸でさえも。",
+  markdown: {
+    anchor: {
+      permalink: false,
+    },
+  },
+};
